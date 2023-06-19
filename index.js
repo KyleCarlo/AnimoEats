09a20144ae38733e6434fe1fad71a69f4370be87
mmonm17 <maria.monica.manlises@gmail.com>
2023-06-20 01:12:50 +0800

    Edited Profile. JSON works now.
    Co-authored-by: Kyle Carlo Lasala <KyleCarlo@users.noreply.github.com>
    Co-authored-by: daphnejanelyngo@gmail.com <daphnejanelyngo@gmail.com>

--- index.js
+++ index.js
@@ -10,13 +10,13 @@ function loadComponents(callback){
     $("#nav-placeholder").load("components/navbar.html");
     $("#card-sample").load("components/card-info.html");
     $(".top-review").load("components/top-review.html");
+    $(".recent-review").load("components/recent-review.html");
 
     $("#map").load("assets/svg/map.html");
 
-    loadTopReviews('json/sample.json');
-
     setTimeout(function(){
-        $(".rating").load("assets/svg/flag.html");
+        loadTopReviews("json/sample.json");
+        $(".review-rating").load("assets/svg/flag.html");
         callback();
     }, 500);
 };
@@ -24,7 +24,6 @@ function loadComponents(callback){
 function hoverPath(){
     var card = $('#bb-holder')[0];
     var bloemen = $('#map svg #blmpath');
-
     /* 
         idList=["stjpath", "facultyC", "scitech", "sports", "murien", "miguel", "blmpath", 
         "yuchengco", "bagno", "connonhall", "henrysy", "lspath", "agnocourt", "johnhall", 
@@ -54,13 +53,61 @@ function hoverPath(){
         $('#bb-holder .billboard p')[0].innerHTML = 'Hover over the map to view more details.';
     });
 };
-    
+
 function loadTopReviews(url){
+    var topReviews = [];
     $.ajax({
         type:"GET",
         url:url,
         success:function(response){
-            console.log(response);
+            topReviews.push(response.top_reviews[0]);
+            topReviews.push(response.top_reviews[1]);
+            topReviews.push(response.top_reviews[2]);
         }
-    })
+    });
+    setTimeout( function(){
+        console.log(topReviews);
+        console.log(topReviews.length);
+        console.log(Math.round(topReviews[0].rating));
+        for(var i = 0; i < topReviews.length; i++){
+            let name = $(".top-review .review-restaurant")[i];
+            let subject = $(".top-review .review-subject")[i];
+            let body = $(".top-review .review-body")[i];
+            let preview = $('.top-review .review-preview');
+            let images; 
+            let rating = ($(".top-review .review-rating svg")[i]);
+            let date = $(".top-review .review-timestamp")[i];
+            let month = {
+                1: "January",
+                2: "February",
+                3: "March",
+                4: "April",
+                5: "May",
+                6: "June",
+                7: "July",
+                8: "August",
+                9: "September",
+                10: "October",
+                11: "November",
+                12: "December",
+            }
+            name.innerHTML = topReviews[i].resto_name;
+            subject.innerHTML = topReviews[i].subject;
+            body.innerHTML = topReviews[i].body;
+            for (var j = 0 ; j < 5; j++){
+                if (j < Math.round(topReviews[i].rating)){
+                    if(Math.round(topReviews[i].rating) < 3){
+                        rating.children[j].style.fill = "#800037";
+                    }
+                    else{
+                        rating.children[j].style.fill = "#008037";
+                    }
+                }
+                else{
+                    rating.children[j].style.fill = "#595959";
+                }
+            }
+            date.innerHTML = "Posted on " +  month[topReviews[i].date.month] + " " + topReviews[i].date.day + ", " + topReviews[i].date.year;
+        }
+    }, 500);
 }
