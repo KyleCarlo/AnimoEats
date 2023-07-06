8cc8c650469b571878b77f81008d74216fd7372f
Kyle Carlo Lasala <kyle_lasala@dlsu.edu.ph>
2023-07-06 22:24:56 +0800

    testing interface

--- index.js
+++ index.js
@@ -5,4 +5,5 @@ $(document).ready(function() {
 function loadComponents(){
     $(".review").load("components/review-card.html");
     $(".store-prev").load("components/store-prev.html");
+    $('.resto-info').load('components/resto-card.html');
 };
