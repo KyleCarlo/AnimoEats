88c24a59129b679e62d931ad99d5a7d54b60a5dc
Sebastian Diaz <62213208+bastidiaz@users.noreply.github.com>
2023-07-06 23:28:19 +0800

    Create "add review" and "view more" buttons
    Added style in style.css
    Included components in index.js

--- index.js
+++ index.js
@@ -6,4 +6,6 @@ function loadComponents(){
     $(".review").load("components/review-card.html");
     $(".store-prev").load("components/store-prev.html");
     $('.resto-info').load('components/resto-card.html');
+    $('.add-review-btn').load('components/add-review-btn.html');
+    $('.view-more-btn').load('components/view-more-btn.html');
 };
