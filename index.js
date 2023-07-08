5af2f32fc9913129cd62f72b13faafce58aa66e3
Kyle Carlo Lasala <kyle_lasala@dlsu.edu.ph>
2023-07-08 14:06:12 +0800

    component making, prof-page progressed
    editing prof-page, component made for rev-self

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-$(document).ready(function() {
-    loadComponents();
-});
+import ReviewCardSelf from "./components/review-card-self.js";
 
-function loadComponents(){
-    $(".review").load("components/review-card.html");
-    $(".store-prev").load("components/store-prev.html");
-    $('.resto-info').load('components/resto-card.html');
-    $('.add-review-btn').load('components/add-review-btn.html');
-    $('.view-more-btn').load('components/view-more-btn.html');
-    $('.side-bar').load('components/side-bar.html');
-    // $('.create-review').load('components/create-review.html');
-};
+window.customElements.define("review-card-self", ReviewCardSelf);
---
+++ index_test.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    loadComponents();
+});
+
+function loadComponents(){
+    $(".review").load("components/review-card.html");
+    $(".store-prev").load("components/store-prev.html");
+    $('.resto-info').load('components/resto-card.html');
+    $('.add-review-btn').load('components/add-review-btn.html');
+    $('.view-more-btn').load('components/view-more-btn.html');
+    $('.side-bar').load('components/side-bar.html');
+    // $('.create-review').load('components/create-review.html');
+};
